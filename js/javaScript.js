ce495327e10c45212535fa64ec881ca4c66180c4
BegoJusto <b.justo002@gmail.com>
2016-11-29 18:05:42 +0100

    formulario

--- js/javaScript.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Curso on 17/11/2016.
- */
---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Curso on 29/11/2016.
+ */
+$.noConflict();
+    jQuery(document).ready(function($){
+        var valor = $("#nombre").val();
+        console.log(valor);
+        }
+    )
