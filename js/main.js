c175b163092b87eca696b45acfac120a10e0bdc1
BegoJusto <b.justo002@gmail.com>
2016-12-02 13:06:22 +0100

    Tabla alumnos falta funciones js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,75 @@
-/**
- * Created by Curso on 29/11/2016.
- */
 $.noConflict();
-    jQuery(document).ready(function($){
-        //tracear();
+var nombres = ["imanol"];
+var apellidos = ["jimenez lopez"];
+var nUF1841 = new Array();
+nUF1841['imanol'] = 5;
+var nUF1842 = [5];
+var nUF1843 = [5];
+var nUF1844 = [5];
+var nUF1845 = [5];
+var nUF1846 = [5];
 
-        function tracear(){
-            var valor = $('#nombre').val();
-            //Nombre
-            console.log(valor);
-            $('#nombre').val("Erasmo");
-            //Nombre
-            valor = $ ('#nombre').attr("value");/*atributo value de html*/
-            //Erasmo
-            console.log(valor);
+jQuery(document).ready(function ($) {
+    function cargarAlumnos() {
+        for (var i = 0; i < nombres.length; i++) {
+            var nombre = nombres[i];
+            var apellido = apellidos[i];
+            var html_text = "<tr>" +
+                "<td><input type='checkbox' value=''/> </td>" +
+                "<td>" + nombre + "</td>" +
+                "<td>" + apellido + "</td>" +
+                "<td>" + nUF1841[nombre] + "</td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td></td>" +
+                "<td><button>Editar</button></td>" +
+                "</tr>";
+            $('#listado-alumnos tbody').append(html_text);
         }
+    }
 
-        /* La funcion no puede ir fuera del ready porque lo lee y lo compila antes de que el documento
-        * esté listo, por lo que #nombre no existe*/
-        $("a.boton").click(function () {
-            var dni =$("#buscador").val();
-            var letra = calcDNI(dni);
-           // console.log(letra) ;
-            $("span.resultado").text(letra);
-            return false;
-        });
-
+    cargarAlumnos();
+    /*
+     function tracear(){
+     // boolean, numericas, texto, Array (Object)
+     var valor = $('#busqueda').val();//
+     //== equivalente if("5"==5) true
+     //=== identico if("5"===5) false
+     // && and || or
+     //!=
+     //!!
+     //Urko
+     // var genero = 1=="1" ? "hombre" : "mujer";
+     console.log(valor);
+     $('#busqueda').val("Erasmo");
+     valor = $('#busqueda').attr("value");//
+     //Urko
+     console.log(valor);
+     //Erasmo
+     valor =   $('#busqueda').val();
+     console.log(valor);
+     }
+     */
+    $("a[href='s1'],a[href='#s2']").click(function (e) {
+        e.preventDefault();
 
     });
-
-function calcDNI(numero){
-    var resto = parseInt(numero % 23);
-    var letras = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"];
-    return letras[resto];
+    $('#productos').find("a.btn").click(function (e) {
+        console.log("pasa");
+        var dni = $('#dni').val();
+        var letra = calcularLetra(parseInt(dni, 10));
+        $('#productos').find("span.resultado").text(letra);
+        e.preventDefault();
+        return false;
+    });
+    //tracear();
+});
+function calcularLetra(numero) {
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+    var letraCalculada;
+    letraCalculada = letras[numero % 23];
+    return letraCalculada;
 }
